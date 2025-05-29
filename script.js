3f32ac9d159b51b9fa42768588e1f042c975e347
Robinairdraktutoriais <81757745+Robinairdraktutoriais@users.noreply.github.com>
2025-05-29 09:23:13 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// deixe aberto = falso;
+// função myFunction(x) {
+// x.classList.toggle("alterar");
+// aberto = !aberto;
+// aberto ? document.getElementsByTagName('nav')[0].style.width = "20%" : document.getElementsByTagName('nav')[0].style.width = "64px" ;
+// deixe elems = document.getElementsByClassName('nav-link');
+// para ( deixe e de elementos ) {
+// aberto ? e.style.display = "bloco" : e.style.display = "nenhum";
+// }
+// }
