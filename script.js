df8b4f2500e8bb8a08e1822a851a8be573a51712
Robinairdraktutoriais <81757745+Robinairdraktutoriais@users.noreply.github.com>
2025-06-12 23:50:46 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// deixe aberto = falso;
-// função myFunction(x) {
-// x.classList.toggle("alterar");
-// aberto = !aberto;
-// aberto ? document.getElementsByTagName('nav')[0].style.width = "20%" : document.getElementsByTagName('nav')[0].style.width = "64px" ;
-// deixe elems = document.getElementsByClassName('nav-link');
-// para ( deixe e de elementos ) {
-// aberto ? e.style.display = "bloco" : e.style.display = "nenhum";
-// }
-// }
+/* No JS ;)
+ *
+ * Icons from entypo.com
+ * Avatar from uifaces.com
+ */
