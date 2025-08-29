94654c6c0efa0c3e8db481a6ee79c613891feb9f
Robinairdraktutoriais <81757745+Robinairdraktutoriais@users.noreply.github.com>
2025-08-29 20:47:50 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-/* No JS ;)
- *
- * Icons from entypo.com
- * Avatar from uifaces.com
- */
+const stripe = Stripe('YOUR_STRIPE_PUBLIC_KEY');
+
+document.getElementById('comprar-produto-1').addEventListener('click', () => {
+ stripe.redirectToCheckout({
+ lineItems: [
+ {
+ price: 'PRICE_ID_PRODUTO_1',
+ quantity: 1,
+ },
+ ],
+ mode: 'payment',
+ successUrl: 'https:                       
+ cancelUrl: '//seusite.com/success',
+ cancelUrl: 'https://seusite.com/cancel',
+ });
+});
+
+document.getElementById('comprar-produto-2').addEventListener('click', () => {
+ stripe.redirectToCheckout({
+ lineItems: [
+ {
+ price: 'PRICE_ID_PRODUTO_2',
+ quantity: 1,
+ },
+ ],
+ mode: 'payment',
+ successUrl: 'https:                       
+ cancelUrl: '//seusite.com/success',
+ cancelUrl: 'https://seusite.com/cancel',
+ });
+});
