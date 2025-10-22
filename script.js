a3be28a12d3f55441f7981cab75801a14184dc29
Robinairdraktutoriais <81757745+Robinairdraktutoriais@users.noreply.github.com>
2025-10-22 19:40:30 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
-const stripe = Stripe('YOUR_STRIPE_PUBLIC_KEY');
-
-document.getElementById('comprar-produto-1').addEventListener('click', () => {
- stripe.redirectToCheckout({
- lineItems: [
- {
- price: 'PRICE_ID_PRODUTO_1',
- quantity: 1,
- },
- ],
- mode: 'payment',
- successUrl: 'https:                       
- cancelUrl: '//seusite.com/success',
- cancelUrl: 'https://seusite.com/cancel',
- });
+document.getElementById('abrir-menu').addEventListener('click', () => {
+    document.getElementById('menu-lateral').classList.add('aberto');
+    document.getElementById('conteudo-principal').classList.add('empurrado');
 });
 
-document.getElementById('comprar-produto-2').addEventListener('click', () => {
- stripe.redirectToCheckout({
- lineItems: [
- {
- price: 'PRICE_ID_PRODUTO_2',
- quantity: 1,
- },
- ],
- mode: 'payment',
- successUrl: 'https:                       
- cancelUrl: '//seusite.com/success',
- cancelUrl: 'https://seusite.com/cancel',
- });
+document.getElementById('fechar-menu').addEventListener('click', () => {
+    document.getElementById('menu-lateral').classList.remove('aberto');
+    document.getElementById('conteudo-principal').classList.remove('empurrado');
 });
