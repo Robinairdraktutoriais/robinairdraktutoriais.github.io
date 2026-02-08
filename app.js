624f1e8dfdd412a6856f361dd17ab3879d87414d
Robinairdraktutoriais <81757745+Robinairdraktutoriais@users.noreply.github.com>
2026-02-08 10:10:52 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-async function startChecker() {
-  if (state.checking) return;
-  state.checking = true;
+function updateStats() {
+  live.textContent = state.live;
+  dead.textContent = state.dead;
+  error.textContent = state.error;
+}
 
-  resetUI();
-
-  const cards = document.getElementById("cards")
-    .value
-    .trim()
-    .split("\n");
-
-  for (const line of cards) {
-    if (!line) continue;
-
-    log(line, "Checking...", "error");
-
-    const res = await checkCard(line);
-
-    if (res.status === "LIVE") state.live++;
-    if (res.status === "DEAD") state.dead++;
-    if (res.status === "ERROR") state.error++;
-
-    log(line, `${res.status} | ${res.msg}`, res.status.toLowerCase());
-    updateStats();
-  }
-
-  state.checking = false;
+function resetUI() {
+  state.live = state.dead = state.error = 0;
+  logBox.innerHTML = "";
+  updateStats();
 }
