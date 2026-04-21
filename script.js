7ff1e1c8898c9e96a6375ea0607d5241a9ed00ae
Robinairdraktutoriais <81757745+Robinairdraktutoriais@users.noreply.github.com>
2026-04-21 12:44:08 -0300

    Excluir script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('abrir-menu').addEventListener('click', () => {
-    document.getElementById('menu-lateral').classList.add('aberto');
-    document.getElementById('conteudo-principal').classList.add('empurrado');
-});
-
-document.getElementById('fechar-menu').addEventListener('click', () => {
-    document.getElementById('menu-lateral').classList.remove('aberto');
-    document.getElementById('conteudo-principal').classList.remove('empurrado');
-});
