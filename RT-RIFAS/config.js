237b496bba4cbfa308b277c67b48184f92713aaa
Robinairdraktutoriais <81757745+Robinairdraktutoriais@users.noreply.github.com>
2026-09-02 17:39:23 -0300

    Atualizar o config.js

--- RT-RIFAS/config.js
+++ RT-RIFAS/config.js
@@ -1,5 +1,6 @@
 // config.js - NÃO COMMITAR ESSE ARQUIVO
+// config.js - NÃO COMMITAR ESSE ARQUIVO
 const CONFIG = {
-  USER: 'admin_taiobeiras', // ← Troca aqui seu usuário
-  PASS_HASH: 'COLOQUE_SEU_HASH_AQUI' // ← Troca aqui
+  USER: 'João',
+  PASS_HASH: 'b42f3e6317e1c8a1b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2'
 };
